b4213ce652a0b35f345bb3fdcfd0df2626e5e4c2
garvinipkiss <garvinipkiss@gmail.com>
2017-12-06 19:03:12 +0300

    initial commit

---
+++ js/login.js
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Francois+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Vollkorn" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+
+    <title>projects</title>
+  </head>
+  <body>
+
+    <div class="sidebar" style="width:25%">
+      <a href="index.html" class="w3"><div class="goli">HOME</div></a>
+      <a href="projects.html" class="w3"><div class="goli">projects</div></a>
+      <a href="about.html" class="w3"><div class="goli">ABOUT</div></a>
+    </div>
+    <div id="projects" class="container">
+      <div class="page-header">
+      <center>  <h1>Projects</h1></center>
+      </div>
+
+
+
+      <div class="well">
+      <ul class="flex-container">
+        <div class="ping">
+          <a href="https://garvinipkiss.github.io/Ping-Pong/"><li class="flex-item">Ping-Pong</li></a>
+        </div>
+        <div class="Anita">
+          <a href="https://garvinipkiss.github.io/Anita-kitchen/"><li class="flex-item">Anita's Kitchen</li></a>
+        </div>
+        <div class="Dice">
+          <a href="https://garvinipkiss.github.io/Dice-game/"><li class="flex-item">Dice</li></a>
+        </div>
+
+        <div class="todo">
+          <a href="#"><li class="flex-item">calc</li></a>
+        </div>
+
+        <div class="bank">
+          <a href="#"><li class="flex-item">walrus</li></a>
+        </div>
+
+        <div class="calc">
+          <a href="#"><li class="flex-item">CALCULATOR</li></a>
+        </div>
+
+        <div class="book">
+          <a href="#"><li class="flex-item">Address-Book</li></a>
+        </div>
+
+
+
+
+
+  </body>
+</html>
