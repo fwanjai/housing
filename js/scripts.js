11198cd691250d441d80dd3f687b4aa1abb3d122
fwanjai <fwanjai@gmail.com>
2017-12-11 15:05:41 +0300

    style for nav bar and body

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function checkScroll() {
+  var startY = $('.navbar').height() * 2; //THe point where the nav-bar changes in pixel
+
+  if ($(window).scrollTop() > startY){
+    $('.navbar').addClass("scrolled");
+  }else {
+    $('.navbar').removeClass("scrolled");
+    }
+  }
+  if ($(.navbar).length > 0) {
+    $(window).on("scroll load resize", function(){
+      checkScroll();
+    });
+  }
