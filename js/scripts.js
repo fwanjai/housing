b8c5321b86bfadab7f8862aa264c25fdb79dd0fe
garvinipkiss <garvinipkiss@gmail.com>
2017-12-13 21:29:40 +0300

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+function checkScroll() {
+  var startY = $('.navbar').height() * 2; //THe point where the nav-bar changes in pixel
+
+  if ($(window).scrollTop() > startY){
+    $('.navbar').addClass("scrolled");
+  }else {
+    $('.navbar').removeClass("scrolled");
+    }
+  }
+  if ($('.navbar').length > 0) {
+    $(window).on("scroll load resize", function(){
+      checkScroll();
+    });
+  }
--- js/uganda.js
+++ js/uganda.js
@@ -24,3 +24,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+function checkScroll() {
+  var startY = $('.navbar').height() * 2; //THe point where the nav-bar changes in pixel
+
+  if ($(window).scrollTop() > startY){
+    $('.navbar').addClass("scrolled");
+  }else {
+    $('.navbar').removeClass("scrolled");
+    }
+  }
+  if ($('.navbar').length > 0) {
+    $(window).on("scroll load resize", function(){
+      checkScroll();
+    });
+  }
