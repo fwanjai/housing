1de873b427252d859bf35e183fb9c00f8ae9260e
fwanjai <fwanjai@gmail.com>
2017-12-14 21:00:22 +0300

    map add

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 function checkScroll() {
   var startY = $('.navbar').height() * 2; //THe point where the nav-bar changes in pixel
 
@@ -11,4 +21,12 @@ function checkScroll() {
     $(window).on("scroll load resize", function(){
       checkScroll();
     });
+  }
+
+  function myMap() {
+  var myCenter = new google.maps.LatLng(41.878114, -87.629798);
+  var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
   }
--- js/tanzania.js
+++ js/tanzania.js
@@ -24,28 +24,25 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+function checkScroll() {
+  var startY = $('.navbar').height() * 2; //THe point where the nav-bar changes in pixel
 
-// Accordion
-function myAccFunc() {
-    var x = document.getElementById("demoAcc");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
+  if ($(window).scrollTop() > startY){
+    $('.navbar').addClass("scrolled");
+  }else {
+    $('.navbar').removeClass("scrolled");
     }
-}
+  }
+  if ($('.navbar').length > 0) {
+    $(window).on("scroll load resize", function(){
+      checkScroll();
+    });
+  }
 
-// Click on the "Jeans" link on page load to open the accordion for demo purposes
-document.getElementById("myBtn").click();
-
-
-// Script to open and close sidebar
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
-
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
+function myMap() {
+var myCenter = new google.maps.LatLng(41.878114, -87.629798);
+var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var marker = new google.maps.Marker({position:myCenter});
+marker.setMap(map);
 }
